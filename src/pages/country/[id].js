2820d29b422f54c984fc6ec20d8a5a5b8ca62bc8
cmajorsmith <cmajorsmith@gmail.com>
2021-03-16 18:47:58 +0200

    Country page refactored

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,98 +1,78 @@
 import Layout from "../../components/Layout/Layout";
-import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
+import {useState} from "react";
 import SearchInput from "../../components/SearchInput/SearchInput";
-const Country=({country,countries, cities})=>{
-  const myCountry = countries.filter(
-    (country1) =>
-      country1.name.includes(country) 
-  );
-  const myCities = cities.filter(
-    (cities) =>
-    cities.country.includes(country) 
-  );
+
+const Country = ({country, countries, cities}) => {
   const [keyword, setKeyword] = useState("");
 
-  const filteredCities = myCities.filter(
-    (cities) =>
-      cities.city.toLowerCase().startsWith(keyword.toLowerCase()) 
-  );
+  // Pick the country from the list by its name
+  const filteredCountries = () => countries.filter((country) => country.name === country);
 
+  // Here we filter out the cities that don't belong to current country
+  const countryCities = cities.filter((city) => city.country.toLowerCase() === country.toLowerCase());
+
+  // Here we filter out the cities that don't match the keyword selected
+  // If there is no keyword selected we simply return the countryCities array as it is
+  const filteredCities = () => {
+    const needle = keyword ? keyword.toLowerCase() : '';
+    return countryCities.filter((city) => city.city.toLowerCase().indexOf(needle) !== -1);
+  };
+
+  // Let's track the input change event and set keyword
+  // Once keyword is set -- it will effect the filteredCities() above
   const onInputChange = (e) => {
     e.preventDefault();
-
-    setKeyword(e.target.value.toLowerCase());
+    setKeyword(e.target.value);
   };
 
   return <Layout countries={countries} title={country}>
-<div className="country-page">
+    <div className="country-page">
       <div className="country-page2">
-      {myCountry.map((countries) =>(
-          
-          <div className="country-h">
-            <div>
-            Discover {countries.name}
+        {filteredCountries().map((country) => (
+            <div className="country-h" key="{country.name}">
+              <div>
+                Discover {country.name}
+              </div>
             </div>
-
-</div>
-      ))}
-        <div className="before-input"> </div>
+        ))}
         <div className="input-div">
-        <SearchInput
-          placeholder="Search for city"
-          onChange={onInputChange}
+          <SearchInput
+              placeholder="Search for city"
+              onChange={onInputChange}
           />
         </div>
-        <div className="before-input"> </div>
-        <div className="btn">
-        <span type="button" className="nav-link filter-cntry" href="#" data-toggle="modal" data-target="#exampleModal2">Filter</span>
-    <div className="modal fade " id="exampleModal2" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-        
-        <span className="btn close-btn modal-item" data-dismiss="modal">Close</span>
-    </div>
-  </div>
-</div>
+
+        <div className="container-fluid cities">
+          <div className="row">
+          {filteredCities().map((city) => (
+            <div className="city-descr col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4" key={city.city}>
+              <Link href={`/city/${city.city}`} key={city.city}>
+                <img className="city-icon" src={city.icon} />
+              </Link>
+              <h1 className="country-city">{city.city}</h1>
+              <p className="country-desc">{city.description}</p>
+            </div>
+          ))}
+          </div>
         </div>
-          <div className="">
-
-          <div className="container-fluid btn cities">
-{filteredCities.map((cities) =>(
-
-<Link href={`/city/${cities.city}`} key={cities.city}>
-
-      <div className="city-descr col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-      <img className="city-icon" src={cities.icon}></img>
-        <h1 className="country-city">{cities.city}</h1>
-        <h1 className="country-desc">{cities.description}</h1>
-        
       </div>
-
-   </Link>
-))}
-   </div>
- </div>
-      </div>
-                 
-        
-</div>
+    </div>
   </Layout>;
 }
 export default Country;
 
-export const getServerSideProps = async({params})=>{
+export const getServerSideProps = async ({params}) => {
   const country = params.id;
   const res = await fetch("https://kristinakuzenko.github.io/cities.json");
   const cities = await res.json();
   const res2 = await fetch("https://kristinakuzenko.github.io/countries.json");
   const countries = await res2.json();
-  return{
-      props:{
-        country,
-        cities,
-        countries
-      },
+  return {
+    props: {
+      country,
+      cities,
+      countries
+    },
   };
 };
