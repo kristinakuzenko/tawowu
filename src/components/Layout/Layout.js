43e6e8222417094f702864949f3b543f26b242c7
kkuzenko <kos.kuzenko@engageft.com>
2021-04-15 21:41:27 +0300

    layout changed

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,42 +5,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import styles from './layout.module.css';
+import { useState, useEffect } from 'react';
+import fire from '../../config/fire-config';
 
 
 const orderBy = (countries) => {
   return countries.sort((a, b) => (a.name > b.name ? 1 : -1));
 };
-const Layout = ({ countries, children, title = "Tawowu" }) => {
-  console.log(countries);
-  const e = countries.filter(
-    (country) =>
-      country.continent.toLowerCase().includes("europe")
-  );
-  const europe = orderBy(e);
-  const australia = countries.filter(
-    (country) =>
-      country.continent.toLowerCase().includes("australia")
-  );
-  const a = countries.filter(
-    (country) =>
-      country.continent.toLowerCase().includes("africa")
-  );
-  const africa = orderBy(a);
-  const n = countries.filter(
-    (country) =>
-      country.continent.toLowerCase().includes("north america")
-  );
-  const northamerica = orderBy(n);
-  const s = countries.filter(
-    (country) =>
-      country.continent.toLowerCase().includes("south america")
-  );
-  const southamerica = orderBy(s);
-  const as = countries.filter(
-    (country) =>
-      country.continent.toLowerCase().includes("asia")
-  );
-  const asia = orderBy(as);
+const Layout = ({ children, title = "Tawowu" }) => {
+  const [countries, setCountries] = useState([]);
+  fire.firestore()
+    .collection('countries')
+    .onSnapshot(snap => {
+      const countries = snap.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setCountries(countries);
+    });
+  const [continents, setContinents] = useState([]);
+  fire.firestore()
+    .collection('continents')
+    .onSnapshot(snap => {
+      const continents = snap.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setContinents(continents);
+    });
+
   const orderedCountries = orderBy(countries);
   return (
     <div className="layout-main">
@@ -65,24 +58,12 @@ const Layout = ({ countries, children, title = "Tawowu" }) => {
               <div className="modal-dialog">
                 <div className="modal-content">
                   <input className="input-search" type="text" placeholder=" Search for cities, countries, ..." ></input>
-                  <Link href={`/continent/Africa`} key="Africa">
-                    <p className="btn modal-item">Africa</p>
-                  </Link>
-                  <Link href={`/continent/Asia`} key="Asia">
-                    <p className="btn modal-item">Asia</p>
-                  </Link>
-                  <Link href={`/continent/Australia`} key="Australia">
-                    <p className="btn modal-item">Oceania</p>
-                  </Link>
-                  <Link href={`/continent/Europe`} key="Europe">
-                    <p className="btn modal-item">Europe</p>
-                  </Link>
-                  <Link href={`/continent/North America`} key="North America">
-                    <p className="btn modal-item">North America</p>
-                  </Link>
-                  <Link href={`/continent/South America`} key="South America">
-                    <p className="btn modal-item">South America</p>
-                  </Link>
+                  {continents.map((continent) => (
+                    <Link href={`/continent/${continent.name} `} key={continent.name}>
+                      <p className="btn modal-item">{continent.name}</p>
+                    </Link>
+                  ))}
+
                   <br></br>
                   <p className="btn modal-item">About</p>
                   <br></br>
@@ -96,82 +77,22 @@ const Layout = ({ countries, children, title = "Tawowu" }) => {
             </button>
             <div className="collapse navbar-collapse " id="navbarResponsive">
               <ul className="navbar-nav ">
-                <li className="nav-item dropdown">
-                  <Link href={`/continent/Africa`} key="Africa">
-                    <a className="nav-link " data-toggle="dropdown"> Africa </a>
-                  </Link>
-                  <ul className="dropdown-menu">
-                    {africa.map((countries) => (
-                      <Link href={`/country/${countries.name}`} key={countries.name}>
-                        <li><a className="dropdown-item btn"> {countries.name}</a></li>
-                      </Link>
-                    ))}
-                  </ul>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link href={`/continent/Asia`} key="Asia">
-                    <a className="nav-link " href="#" data-toggle="dropdown"> Asia </a>
-                  </Link>
-                  <ul className="dropdown-menu">
-                    {asia.map((countries) => (
-                      <Link href={`/country/${countries.name}`} key={countries.name}>
-                        <li><a className="dropdown-item btn"> {countries.name}</a></li>
-                      </Link>
-                    ))}
-                  </ul>
-                </li>
-          
+                {continents.map((continent) => (
+                  <li className="nav-item dropdown">
+                    <Link href={`/continent/${continent.name} `} key={continent.name}>
+                      <a className="nav-link " data-toggle="dropdown"> {continent.name} </a>
+                    </Link>
+                    <ul className="dropdown-menu">
+                      {countries.filter((country) => country.continent.toLowerCase() == continent.name.toLowerCase()).map((countries) => (
+                        <Link href={`/country/${countries.name}`} key={countries.name}>
+                          <li><a className="dropdown-item btn"> {countries.name}</a></li>
+                        </Link>
+                      ))}
 
-                <li className="nav-item dropdown">
-                  <Link href={`/continent/Europe`} key="Europe">
-                    <a className="nav-link " href="#" data-toggle="dropdown"> Europe </a>
-                  </Link>
-                  <ul className="dropdown-menu">
-                    {europe.map((countries) => (
-                      <Link href={`/country/${countries.name}`} key={countries.name}>
-                        <li><a className="dropdown-item btn"> {countries.name}</a></li>
-                      </Link>
-                    ))}
-                  </ul>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link href={`/continent/North America`} key="North America">
-                    <a className="nav-link " href="#" data-toggle="dropdown"> NorthAmerica </a>
-                  </Link>
-                  <ul className="dropdown-menu">
-                    {northamerica.map((countries) => (
-                      <Link href={`/country/${countries.name}`} key={countries.name}>
-                        <li><a className="dropdown-item btn"> {countries.name}</a></li>
-                      </Link>
-                    ))}
-                  </ul>
-                </li>
+                    </ul>
+                  </li>
+                ))}
 
-                <li className="nav-item dropdown">
-                  <Link href={`/continent/Australia`} key="Australia">
-                    <a className="nav-link " href="#" data-toggle="dropdown"> Oceania </a>
-                  </Link>
-                  <ul className="dropdown-menu">
-                    {australia.map((countries) => (
-                      <Link href={`/country/${countries.name}`} key={countries.name}>
-                        <li><a className="dropdown-item btn"> {countries.name}</a></li>
-                      </Link>
-                    ))}
-                  </ul>
-                </li>
-                
-                <li className="nav-item dropdown">
-                  <Link href={`/continent/South America`} key="South America">
-                    <a className="nav-link " href="#" data-toggle="dropdown"> SouthAmerica </a>
-                  </Link>
-                  <ul className="dropdown-menu">
-                    {southamerica.map((countries) => (
-                      <Link href={`/country/${countries.name}`} key={countries.name}>
-                        <li><a className="dropdown-item btn"> {countries.name}</a></li>
-                      </Link>
-                    ))}
-                  </ul>
-                </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">About </a>
                 </li>
@@ -181,12 +102,6 @@ const Layout = ({ countries, children, title = "Tawowu" }) => {
           </div>
         </nav>
       </header>
-
-
-
-
-
-
 
       <main className={styles.main}>{children}</main>
 
--- src/pages/continent/[id].js
+++ src/pages/continent/[id].js
@@ -2,25 +2,25 @@ import Layout from "../../components/Layout/Layout";
 import Link from "next/link";
 //database import
 import fire from '../../config/fire-config';
-import {useState} from "react";
+import { useState } from "react";
 
 const orderBy = (countries) => {
-  return countries.sort((a, b) => (a.name> b.name ? 1 : -1));
+  return countries.sort((a, b) => (a.name > b.name ? 1 : -1));
 };
-const Continent=({continent})=>{
+const Continent = ({ continent }) => {
   const [countries, setCountries] = useState([]);
   fire.firestore()
-      .collection('countries')
-      .onSnapshot(snap => {
-        const countries = snap.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
-        setCountries(countries);
-});
+    .collection('countries')
+    .onSnapshot(snap => {
+      const countries = snap.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setCountries(countries);
+    });
 
-const [continents, setContinents] = useState([]);
-fire.firestore()
+  const [continents, setContinents] = useState([]);
+  fire.firestore()
     .collection('continents')
     .onSnapshot(snap => {
       const continents = snap.docs.map(doc => ({
@@ -28,64 +28,63 @@ fire.firestore()
         ...doc.data()
       }));
       setContinents(continents);
-});
+    });
   const myCountry = countries.filter(
     (country1) =>
-      country1.continent.includes(continent) 
+      country1.continent.includes(continent)
   );
   const myContinent = continents.filter(
     (country1) =>
-      country1.name.includes(continent) 
+      country1.name.includes(continent)
   );
-  const orderedCountries=orderBy(myCountry);
-  return <Layout countries={countries} title={continent}>
+  const orderedCountries = orderBy(myCountry);
+  return <Layout title={continent}>
 
+    <div className="continent2">
 
-<div className="continent2">
+      {myContinent.map((continents) => (
+        <div >
+          <div className="main-header2">
+            <div>
+              <span>Welcome </span>
+              <span>to </span>
+              <span> {continents.name}</span>
+            </div>
 
-{myContinent.map((continents) =>(
-            <div >   
-        <div className="main-header2">
-          <div>
-          <span>Welcome </span>
-          <span>to </span>
-          <span> {continents.name}</span>
-          </div>
-       
-          <h2><button type="button" className="btn discover2">Choose the country</button></h2>
-  
+            <h2><button type="button" className="btn discover2">Choose the country</button></h2>
+
           </div>
           <div className="contour">
-          <img className="" src={continents.contour} ></img>
-            </div>
+            <img className="" src={continents.contour} ></img>
+          </div>
 
-            </div>
-        ))}
-      
-</div>
-<div className="countries container-fluid btn">
-        {orderedCountries.map((countries) =>(
-           <Link href={`/country/${countries.name}`} key={countries.name}>
-      <div className="city-descr col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-      <img src={countries.icon}/>
-        <h1 className="country-city">{countries.name}</h1>
-      </div>
-           </Link>
-          
-        ))}
+        </div>
+      ))}
+
+    </div>
+    <div className="countries container-fluid btn">
+      {orderedCountries.map((countries) => (
+        <Link href={`/country/${countries.name}`} key={countries.name}>
+          <div className="city-descr col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+            <img src={countries.icon} />
+            <h1 className="country-city">{countries.name}</h1>
+          </div>
+        </Link>
+
+      ))}
 
 
-</div>
+    </div>
 
   </Layout>;
 
 }
 export default Continent;
-export const getServerSideProps = async({params})=>{
-    const continent = params.id;
-    return{
-        props:{
-          continent
-        },
-    };
+export const getServerSideProps = async ({ params }) => {
+  const continent = params.id;
+  return {
+    props: {
+      continent
+    },
   };
+};
